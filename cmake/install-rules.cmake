a24e2cf8832e238d78ed9e5ad5bcd59d194c428f
JesusKrists <krists.n99@gmail.com>
2024-09-22 22:49:55 +0200

    Automate release

--- cmake/install-rules.cmake
+++ cmake/install-rules.cmake
@@ -1,7 +1,4 @@
-install(
-    TARGETS CodeTask_exe
-    RUNTIME COMPONENT CodeTask_Runtime
-)
+install(TARGETS CodeTask_exe DESTINATION ${INSTALL_PATH})
 
 if(PROJECT_IS_TOP_LEVEL)
   include(CPack)
--- cmake/variables.cmake
+++ cmake/variables.cmake
@@ -1,9 +1,9 @@
 # ---- Developer mode ----
 
 # Developer mode enables targets and code paths in the CMake scripts that are
-# only relevant for the developer(s) of CodeTask
-# Targets necessary to build the project must be provided unconditionally, so
-# consumers can trivially build and package the project
+# only relevant for the developer(s) of CodeTask Targets necessary to build the
+# project must be provided unconditionally, so consumers can trivially build and
+# package the project
 if(PROJECT_IS_TOP_LEVEL)
   option(CodeTask_DEVELOPER_MODE "Enable developer mode" OFF)
 endif()
@@ -11,18 +11,17 @@ endif()
 # ---- Warning guard ----
 
 # target_include_directories with the SYSTEM modifier will request the compiler
-# to omit warnings from the provided paths, if the compiler supports that
-# This is to provide a user experience similar to find_package when
-# add_subdirectory or FetchContent is used to consume this project
+# to omit warnings from the provided paths, if the compiler supports that This
+# is to provide a user experience similar to find_package when add_subdirectory
+# or FetchContent is used to consume this project
 set(warning_guard "")
 if(NOT PROJECT_IS_TOP_LEVEL)
-  option(
-      CodeTask_INCLUDES_WITH_SYSTEM
-      "Use SYSTEM modifier for CodeTask's includes, disabling warnings"
-      ON
-  )
+  option(CodeTask_INCLUDES_WITH_SYSTEM
+         "Use SYSTEM modifier for CodeTask's includes, disabling warnings" ON)
   mark_as_advanced(CodeTask_INCLUDES_WITH_SYSTEM)
   if(CodeTask_INCLUDES_WITH_SYSTEM)
     set(warning_guard SYSTEM)
   endif()
 endif()
+
+set(INSTALL_PATH ${CMAKE_SOURCE_DIR}/bin)
